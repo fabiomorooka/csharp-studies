2a94b7865f7c0d9866d174d29113d0cb44139f31
fabio.morooka <fabio.morooka@gmail.com>
2022-11-27 11:50:06 -0300

    update dependency injection

--- Projetos/CalculadoraV2/CalculatorSystem/BasicCalculator.cs
+++ Projetos/CalculadoraV2/CalculatorSystem/BasicCalculator.cs
@@ -6,20 +6,10 @@ namespace CalculatorV2.CalculatorSystem
 {
     public class BasicCalculator
     {
-        private readonly IAddition _addition;
-        private readonly ISubtraction _subtraction;
-        private readonly IMultiplication _multiplication;
-        private readonly IDivision _division;
-        private readonly IExponentiation _exponentiation;
-        private readonly IRadication _radication;
+        private readonly ICalculatorContext _context;
         public BasicCalculator(ICalculatorContext context)
         {
-            _addition = context.Addition;
-            _subtraction = context.Subtraction;
-            _multiplication = context.Multiplication;
-            _division = context.Division;
-            _exponentiation = context.Exponentiation;
-            _radication = context.Radication;
+            _context = context;
         }
 
         public double DoOperation(double num1, double num2, string operation)
@@ -29,24 +19,24 @@ namespace CalculatorV2.CalculatorSystem
             switch ((OperationsEnum)(int.Parse(operation)))
             {
                 case OperationsEnum.Add:
-                    result = _addition.Add(num1, num2);
+                    result = _context.Addition.Add(num1, num2);
                     break;
                 case OperationsEnum.Subtract:
-                    result = _subtraction.Subtract(num1, num2);
+                    result = _context.Subtraction.Subtract(num1, num2);
                     break;
                 case OperationsEnum.Multiply:
-                    result = _multiplication.Multiply(num1, num2);  
+                    result = _context.Multiplication.Multiply(num1, num2);  
                     break;
                 case OperationsEnum.Divide:
                     if (num2 != 0)
-                        result = _division.Divide(num1, num2);
+                        result = _context.Division.Divide(num1, num2);
                     break;
                 case OperationsEnum.Exponentiation:
-                    result = _exponentiation.Exponent(num1, num2);
+                    result = _context.Exponentiation.Exponent(num1, num2);
                     break;
                 case OperationsEnum.Radication:
                     if (num2 != 0)
-                        result = _radication.Radicate(num1, num2);
+                        result = _context.Radication.Radicate(num1, num2);
                     break;
                 default:
                     break;
